5090ff34771201e137f789d4bcd995e5eb15dc69
Claudiazepam <martinezallendesclaudia@gmail.com>
2024-07-22 19:14:13 -0400

    logrado

---
+++ index.js
@@ -0,0 +1,46 @@
+
+
+/*Desarrollo Ejercicio*/
+
+const precio = 400000
+let  cantidad = 0
+
+
+/*Casilla Precio*/
+precioSpan = document.querySelector(".precio-inicial")
+precioSpan.innerHTML = precio
+
+/*Casilla Cantidad*/
+cantidadSpan = document.querySelector(".cantidad")
+cantidadSpan.innerHTML = cantidad
+
+/*Casilla total a pagar*/
+totalSpan = document.querySelector(".valor-total")
+totalSpan.innerHTML = precio * cantidad
+
+
+/*Botones*/
+function incrementar() {
+    cantidad++
+    cantidadSpan.innerHTML = cantidad
+    totalfinal()
+
+}
+
+function disminuir() {
+    cantidadSpan.innerHTML = cantidad--
+totalfinal()
+}
+
+/*Actualizar total*/
+function totalfinal() {
+    totalSpan.innerHTML = cantidad * precio
+
+}
+
+
+
+
+
+
+
